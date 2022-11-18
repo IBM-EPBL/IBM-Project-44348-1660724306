06c75e7a590d581dea03bd591c872d59556ad5da
Sobiyasobi <113794143+Sobiyasobi@users.noreply.github.com>
2022-11-18 11:55:01 +0530

    Add files via upload

---
+++ Setting up application environment/flask project/app.py
@@ -0,0 +1,9 @@
+
+from flask import Flask,render_template,request
+app=Flask(__name__)
+@app.route('/')
+def hello world():
+    return ("HELLO WORLD")
+if__name__=={}    
+if __name__==('__main__'):
+    app.run(debug=True)
